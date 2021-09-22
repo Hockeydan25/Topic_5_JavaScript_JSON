26d76be187e2788f2e86378d9b5ddbcdb6972601
Dan Smestad <hockeydan25@gmail.com>
2021-09-22 06:57:22 -0500

    setup new code for topic 5 java and node.js setup

---
+++ functions.js
@@ -0,0 +1,7 @@
+function shout(text) {
+    let shouty_text = text.toUpperCase() + '!!!'
+    return shouty_text
+}
+
+console.log(shout('hello world'))
+let message = shout('hello web programmers')
---
+++ hello.js
@@ -0,0 +1,26 @@
+console.log('hello World!')
+
+//Three ways to write creat variables
+let color = 'blue'
+var size = 'medium' //scope is not recommended scope is much wider
+const language = 'Javascript' //can't change this value
+
+
+let quantity = 6
+let distance = 4.5
+
+let text = 'Hello World'
+let message = "Hi Programmers"
+
+console.log('There are ' + quantity + ' programmers ' )
+
+let todayTemp = 75
+
+//create message"Today's temperature is 75F"
+
+console.log('Today\'s temperature is '+ todayTemp + 'F')
+
+let tempC = (todayTemp -32) * 5 / 9
+console.log('Today\'s temperature is ' + tempC.toFixed(2)+ 'C')
+
+
